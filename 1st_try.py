0cf08a3cadc0279194762cf3de44224cb9f288f6
Harry Lin <b08901002@ntu.edu.tw>
2024-01-09 15:22:46 +0800

    adjust window size and show at the center

--- 1st_try.py
+++ 1st_try.py
@@ -5,7 +5,18 @@ def on_button_click():
 
 # create main window
 root = tk.Tk()
-root.title("Simple App")
+root.title("Sakamichi member's blog")
+
+#get the width,height of computer's screen
+#so that window can show at its center
+window_width = 1200
+window_height = 600
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight() - 40 #minus the height(40 or 75) of taskbar below the screen
+center_x = int(screen_width/2 - window_width/2)
+center_y = int(screen_height/2 - window_height/2)
+#set W,H of the window
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
 # create a label
 label = tk.Label(root, text="按下按鈕")
